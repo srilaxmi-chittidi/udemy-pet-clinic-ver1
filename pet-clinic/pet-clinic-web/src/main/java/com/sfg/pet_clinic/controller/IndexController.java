93f35152202f46b96d945e3319484e69aee68d5b
srilaxmi-chittidi <laxmi.chittidi@gmail.com>
2024-12-17 13:34:09 -0500

    Add Pet Clinic Index page and Controller closes #8

---
+++ pet-clinic/pet-clinic-web/src/main/java/com/sfg/pet_clinic/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.sfg.pet_clinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({"/","","index","index.html"})
+	public String getIndex(Model model) {
+		
+	//	model.addAttribute("Index Page");
+		
+		return "index";
+	}
+}
