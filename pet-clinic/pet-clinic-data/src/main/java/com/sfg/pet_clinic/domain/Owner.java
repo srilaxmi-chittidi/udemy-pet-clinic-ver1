fb95edd1041913e009a52f88704c25a37137d7ee
srilaxmi-chittidi <laxmi.chittidi@gmail.com>
2024-12-20 21:06:41 -0500

    Create Pet type , Pet and Visit Entities closes #26

--- pet-clinic/pet-clinic-data/src/main/java/com/sfg/pet_clinic/domain/Owner.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/sfg/pet_clinic/domain/Owner.java
@@ -1,9 +1,16 @@
 package com.sfg.pet_clinic.domain;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
-	/**
-	 * 
-	 */
+	private Set<PetType> pets;
 
+	public Set<PetType> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<PetType> pets) {
+		this.pets = pets;
+	}
 }
