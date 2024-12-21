dc5b2170119cafde7f03e248a039b7682697254c
srilaxmi-chittidi <laxmi.chittidi@gmail.com>
2024-12-20 21:15:35 -0500

    Create Vet Specialty Entity, Associate to Vet closes  #27

---
+++ pet-clinic/pet-clinic-data/src/main/java/com/sfg/pet_clinic/domain/Specialty.java
@@ -0,0 +1,14 @@
+package com.sfg.pet_clinic.domain;
+
+public class Specialty extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic/pet-clinic-data/src/main/java/com/sfg/pet_clinic/domain/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/sfg/pet_clinic/domain/Vet.java
@@ -1,10 +1,17 @@
 package com.sfg.pet_clinic.domain;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	private Set<Specialty> specialties;
 
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
